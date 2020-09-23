d3b02239a014761d74a0f0983dbbbb15d206a8b1
Ryan G. Marcoux <rmarcoux1@gmail.com>
2020-09-22 20:30:47 -0400

    initial commit

---
+++ src/main/java/com/projects/irrigationcontrollerbatch/IrrigationControllerBatchApplication.java
@@ -0,0 +1,13 @@
+package com.projects.irrigationcontrollerbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrrigationControllerBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrrigationControllerBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/irrigationcontrollerbatch/IrrigationControllerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.irrigationcontrollerbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrrigationControllerBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
